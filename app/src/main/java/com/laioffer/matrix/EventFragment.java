41ef705245ab1370b78afb1e1751acd5d81e8ebd
Qihao He <qi.he@maine.edu>
2019-07-20 00:57:02 -0700

    Android Activity and Fragment

---
+++ app/src/main/java/com/laioffer/matrix/EventFragment.java
@@ -0,0 +1,50 @@
+package com.laioffer.matrix;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventFragment extends Fragment {
+
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null (which
+     * is the default implementation).  This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+     *
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the View for the fragment's UI, or null.
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.event_list);
+        listView.setAdapter(new EventAdapter(getActivity()));
+        return view;
+    }
+}
