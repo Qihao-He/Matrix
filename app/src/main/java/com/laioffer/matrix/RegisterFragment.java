36b35729bbf649922ea077aedca15605fc924c34
Qihao He <qi.he@maine.edu>
2019-07-23 21:25:57 -0700

    android view and onBoarding

---
+++ app/src/main/java/com/laioffer/matrix/RegisterFragment.java
@@ -0,0 +1,48 @@
+package com.laioffer.matrix;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends OnBoardingBaseFragment {
+
+    public static RegisterFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        RegisterFragment fragment = new RegisterFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        submitButton.setText(getString(R.string.register));
+        return view;
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_register;
+    }
+
+
+}
+
