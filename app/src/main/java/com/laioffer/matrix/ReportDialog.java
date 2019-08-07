47c0ede0a9917ad95bb4e964e262a5a47045f19c
Qihao He <qi.he@maine.edu>
2019-08-06 20:39:19 -0700

    Apply Anmiation Effect

--- app/src/main/java/com/laioffer/matrix/ReportDialog.java
+++ app/src/main/java/com/laioffer/matrix/ReportDialog.java
@@ -1,15 +1,20 @@
 package com.laioffer.matrix;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Dialog;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.Window;
 
 import androidx.annotation.NonNull;
 
 public class ReportDialog extends Dialog {
+    private int cx;
+    private int cy;
 
     public ReportDialog(@NonNull Context context) {
         this(context, R.style.MyAlertDialogStyle);
@@ -17,6 +22,14 @@ public class ReportDialog extends Dialog {
 
     public ReportDialog(@NonNull Context context, int themeResId) {
         super(context, themeResId);
+    }
+
+    public static ReportDialog newInstance(Context context, int cx, int cy) {
+
+        ReportDialog dialog = new ReportDialog(context, R.style.MyAlertDialogStyle);
+        dialog.cx = cx;
+        dialog.cy = cy;
+        return dialog;
     }
 
     @Override
@@ -27,4 +40,34 @@ public class ReportDialog extends Dialog {
         setContentView(dialogView);
         getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
     }
+
+    private void animateDialog(View dialogView, boolean open) {
+        final View view = dialogView.findViewById(R.id.dialog);
+        int w = view.getWidth();
+        int h = view.getHeight();
+
+        int endRadius = (int) Math.hypot(w, h);
+
+        if (open) {
+            Animator revealAnimator = ViewAnimationUtils.createCircularReveal(view, cx, cy, 0, endRadius);
+            view.setVisibility(View.VISIBLE);
+            revealAnimator.setDuration(500);
+            revealAnimator.start();
+        } else {
+            Animator anim = ViewAnimationUtils.createCircularReveal(view, cx, cy, endRadius, 0);
+
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    view.setVisibility(View.INVISIBLE);
+                    dismiss();
+
+                }
+            });
+            anim.setDuration(500);
+            anim.start();
+        }
+    }
+
 }
