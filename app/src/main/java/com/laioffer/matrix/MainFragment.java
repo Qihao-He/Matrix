377f510e8b0b5f7d52d212fbb520a87a92a680d5
Qihao He <qi.he@maine.edu>
2019-08-15 19:54:25 -0700

    Not only static permission, but also dynamic permission.

--- app/src/main/java/com/laioffer/matrix/MainFragment.java
+++ app/src/main/java/com/laioffer/matrix/MainFragment.java
@@ -1,13 +1,17 @@
 package com.laioffer.matrix;
 
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
@@ -63,6 +67,25 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
     private FirebaseStorage storage;
     private StorageReference storageRef;
 
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 
     public static MainFragment newInstance() {
 
